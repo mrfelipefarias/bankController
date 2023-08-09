e4479ff03688153c4b3c20ae603551e58cf432ad
mrfelipefarias <mrfelipefarias@hotmail.com>
2023-08-09 18:34:14 -0300

    iniciando o projeto

---
+++ src/main/java/com/example/bankController/BankControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankControllerApplication.class, args);
+	}
+
+}
